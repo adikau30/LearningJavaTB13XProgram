9c97bb29790fa9fd22f83439ca65960f6fef07a3
aditya kaushik <adi.kau30@gmail.com>
2025-06-10 01:39:11 +0530

    Lab004

---
+++ src/ex_01_Java_Basic/Lab004.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Lab004 {
+    public static void main(String[] args) {
+//        This is comment added to check greyout comment
+        System.out.println("Hi there");
+    }
+}
