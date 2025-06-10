4ea6ee9d46b98ebfc92853618ecf8e3cc55204e5
aditya kaushik <adi.kau30@gmail.com>
2025-06-10 23:57:18 +0530

    Lab020_9multiply

---
+++ src/ex_01_Java_Basic/lab010_9multiply.java
@@ -0,0 +1,35 @@
+package ex_01_Java_Basic;
+
+public class lab010_9multiply {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int e = 5;
+        int f = 6;
+        int g = 7;
+        int h = 8;
+        int i = 9;
+        int j = 10;
+        System.out.printf("%d * %d = %d",i,a, i * a);
+        System.out.println();
+        System.out.printf("%d * %d = %d",i,b, i * b);
+        System.out.println();
+        System.out.printf("%d * %d = %d",i,c, i * c);
+        System.out.println();
+        System.out.printf("%d * %d = %d",i,d, i * d);
+        System.out.println();
+        System.out.printf("%d * %d = %d",i,e, i * e);
+        System.out.println();
+        System.out.printf("%d * %d = %d",i,f, i * f);
+        System.out.println();
+        System.out.printf("%d * %d = %d",i,g, i * g);
+        System.out.println();
+        System.out.printf("%d * %d = %d",i,h, i * h);
+        System.out.println();
+        System.out.printf("%d * %d = %d",i,i, i * i);
+        System.out.println();
+        System.out.printf("%d * %d = %d",i,j, i * j);
+    }
+}
