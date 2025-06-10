fa848b5544902b0582a9e9508397b753dde84277
aditya kaushik <adi.kau30@gmail.com>
2025-06-10 23:05:06 +0530

    Lab007_variables

---
+++ src/ex_01_Java_Basic/lab007_boolean.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+import java.security.KeyStore;
+
+public class lab007_boolean {
+    public static void main(String[] args) {
+        boolean istes = true;
+        System.out.println(istes);
+    }
+}
