7caa281aa6fc9a3d725450cdd82fb9880d02d013
aditya kaushik <adi.kau30@gmail.com>
2025-06-10 23:38:21 +0530

    Lab009

---
+++ src/ex_01_Java_Basic/lab009_printf.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+public class lab009_printf {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.printf("The value of a and b is %d , %d", a , b);
+    }
+}
