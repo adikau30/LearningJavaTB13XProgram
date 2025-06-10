0dc0fda312814278b34c18d69e99c27e186890b6
aditya kaushik <adi.kau30@gmail.com>
2025-06-11 00:09:44 +0530

    Lab012_c

--- src/ex_01_Java_Basic/lab012_constants.java
+++ src/ex_01_Java_Basic/lab012_constants.java
@@ -4,6 +4,8 @@ public class lab012_constants {
     public static void main(String[] args) {
         final int  a = 10;
         System.out.println(a);
+        final float pi= 3.14f;
+        System.out.println(pi);
 
     }
 }
