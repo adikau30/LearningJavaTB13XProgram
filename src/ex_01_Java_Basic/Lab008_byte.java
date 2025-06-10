514889b96af87d8b1dbc520652c0f33ad109c7dc
aditya kaushik <adi.kau30@gmail.com>
2025-06-10 23:09:00 +0530

    Lab_008_byte

---
+++ src/ex_01_Java_Basic/Lab008_byte.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basic;
+
+public class Lab008_byte {
+    public static void main(String[] args) {
+        byte b = 100;
+        // b=128;
+        // b= -127;
+        int i = 10;
+        short s = 90;
+        char c= 'a';
+        long l= 08766677l;
+        long l1= 98899000L;
+    }
+}
