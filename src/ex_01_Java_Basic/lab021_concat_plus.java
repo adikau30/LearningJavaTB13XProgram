bba5719d9e8b629bff3e3684b1121b64d14a0b02
aditya kaushik <adi.kau30@gmail.com>
2025-06-12 01:20:23 +0530

    Lab020

---
+++ src/ex_01_Java_Basic/lab021_concat_plus.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basic;
+
+public class lab021_concat_plus {
+    public static void main(String[] args) {
+        // concatination
+        int a= 10;
+        int b= 20;
+        System.out.println(a+b);
+        System.out.println("aditya" + " "+ "kaushik");
+
+        // + -> behave differently with data type
+        // + -> operator overloading
+    }
+}
