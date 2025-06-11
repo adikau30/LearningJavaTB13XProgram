3a38ad478acf952d5d63b1a83fdeb65cb76f066d
aditya kaushik <adi.kau30@gmail.com>
2025-06-12 00:56:07 +0530

    Lab015_multiply9

---
+++ src/ex_01_Java_Basic/lab017_operators.java
@@ -0,0 +1,21 @@
+package ex_01_Java_Basic;
+
+public class lab017_operators {
+    public static void main(String[] args) {
+        // arthematic operators
+        // + addition
+        // - substraction
+        // * multiplication
+        // / divison
+        // % Modulus- Remainder
+        int a= 20;
+        int b=3;
+        float c=3.14f;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a/c);
+        System.out.println(a%b);
+    }
+}
---
+++ src/ex_01_Java_Basic/lab018_operator.java
@@ -0,0 +1,19 @@
+package ex_01_Java_Basic;
+
+public class lab018_operator {
+    public static void main(String[] args) {
+        // unary operator
+        int a= +10;
+        // int a = 10 optional +
+        // but - is not
+        int a1 = -110;
+                int result= a+a1;
+        System.out.println(a);
+        System.out.println(a1);
+        System.out.println( result );
+
+        int b= -1;
+                b = b+1;
+        System.out.println(b);
+    }
+}
