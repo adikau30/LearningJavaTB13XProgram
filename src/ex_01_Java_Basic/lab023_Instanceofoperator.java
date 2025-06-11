4f862f848edbb3558d050437fcca71ee2da9e861
aditya kaushik <adi.kau30@gmail.com>
2025-06-12 01:52:11 +0530

    Lab024

---
+++ src/ex_01_Java_Basic/lab023_Instanceofoperator.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+import java.util.SplittableRandom;
+
+public class lab023_Instanceofoperator {
+    public static void main(String[] args) {
+        String s1= new String( "Aditya");
+        System.out.println(s1 instanceof String);
+    }
+}
