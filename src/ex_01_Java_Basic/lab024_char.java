6383e65435cb3dbe8920634d7f8d6bb5ac20f9ff
aditya kaushik <adi.kau30@gmail.com>
2025-06-12 01:54:42 +0530

    Lab024

---
+++ src/ex_01_Java_Basic/lab024_char.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+public class lab024_char {
+    public static void main(String[] args) {
+        char a1 = 'A';
+        char b1= 'B';
+        System.out.println(a1+b1);
+    }
+}
