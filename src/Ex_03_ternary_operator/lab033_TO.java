a7a5ce08c42eb0389265c1b00d2ca38e9af9c98b
aditya kaushik <adi.kau30@gmail.com>
2025-06-12 15:40:48 +0530

    Lab033

---
+++ src/Ex_03_ternary_operator/lab033_TO.java
@@ -0,0 +1,10 @@
+package Ex_03_ternary_operator;
+
+public class lab033_TO {
+    public static void main(String[] args) {
+        //even or odd
+        int num= 10;
+        String result= (num%2 ==0)? "even":"odd";
+        System.out.println(result);
+    }
+}
