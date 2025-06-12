224e9bd8b9fe37d3135ab90909e50157ebb70de1
aditya kaushik <adi.kau30@gmail.com>
2025-06-12 14:29:55 +0530

    Lab028

---
+++ src/Ex_01_Typecasting/lab028_typecasting.java
@@ -0,0 +1,11 @@
+package Ex_01_Typecasting;
+
+public class lab028_typecasting {
+    public static void main(String[] args) {
+        long phoneno = 9450421748l;
+        //short s= phone
+        // implicit narrow not allowed
+        short s1= (short)phoneno;
+        System.out.println(s1);
+    }
+}
