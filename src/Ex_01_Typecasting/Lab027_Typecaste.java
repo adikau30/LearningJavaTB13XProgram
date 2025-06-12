56db8b34915cb50f089c2790faa5856e05f6c3b3
aditya kaushik <adi.kau30@gmail.com>
2025-06-12 13:56:21 +0530

    Lab027_Typecaste.java

---
+++ src/Ex_01_Typecasting/Lab027_Typecaste.java
@@ -0,0 +1,13 @@
+package Ex_01_Typecasting;
+
+public class Lab027_Typecaste {
+    public static void main(String[] args) {
+       byte b = 10;
+       int a=b;
+       //widening -> Implicit widening
+        int a1= (int)b;
+        //widening -> explicit casting
+        System.out.println(a);
+        System.out.println(a1);
+    }
+}
