84aa2b869ba711386d8830d9bb81b2caebea7186
aditya kaushik <adi.kau30@gmail.com>
2025-06-12 15:38:13 +0530

    Lab032

---
+++ src/Ex_03_ternary_operator/lab032_TO.java
@@ -0,0 +1,12 @@
+package Ex_03_ternary_operator;
+
+public class lab032_TO {
+    public static void main(String[] args) {
+        // maximun munber between two numbers by using ternary operators
+        int x= 10;
+        int y= 20;
+        //System.out.println(Math.max(x,y));
+        int max= x>y ? x:y;
+        System.out.println(max);
+    }
+}
